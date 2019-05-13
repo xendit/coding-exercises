321dcd2f81cd90c7bb126eb9a2185f8a902785d7
Bo Chen <bochen303@gmail.com>
2019-05-13 15:43:11 +0700

    update date util

--- Tooling/06. Code Coverage Tools/src/date_util.js
+++ Tooling/06. Code Coverage Tools/src/date_util.js
@@ -2,10 +2,25 @@
 
 class DateUtil {
     static getTomorrow() {
-        const now = new Date();
-        now.setDate(now.getDate() + 1);
+        const now = new Date(); const num = 1;
+        now.setDate(now.getDate() + num);
 
         return now;
+    }
+
+    static formatDate(now) {
+        const isDateEven = now.getDate() % 2 === 0;
+        const isMonthEven = now.getMonth() % 2 === 0;
+
+        if (isDateEven && isMonthEven) {
+            return 'even';
+        } else if (isDateEven) {
+            return 'only date even';
+        } else if (isMonthEven) {
+            return 'only month even';
+        } else {
+            return 'all odd';
+        }
     }
 }
 
