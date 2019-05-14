253f7e8d7a8f4f7488372d41dc22c6d59932c22e
Fauzan Hilmi <fauzanhilmir@gmail.com>
2019-05-14 14:13:38 +0700

    Add more endpoints

--- Testing/02. Testing Imperative Logic/tests/mocks/mock_server.js
+++ Testing/02. Testing Imperative Logic/tests/mocks/mock_server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const port = 1234;
 
-app.get('*', (req, res) => res.send('<html><head></head><body><div>Hello<div>World</div></div></body>'));
+app.get('/cats', (req, res) => res.send('<html><head></head><body><h1><span>Meow<span></h1><h2>meow</h2><ul><li>1</li><li>2</li></ul></body></html>'));
+app.get('/dogs', (req, res) => res.send('<html><head></head><body><div>Woof<div>woof</div></div></body></html>'));
 
 app.listen(port, () => console.log(`Mock server listening on port ${port}!`));
